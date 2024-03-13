068483fe3a23797bcc7a5e7becf5fdcaf689d052
SinIn4Tech <phungxuangiang.hanoi@gmail.com>
2024-03-13 22:52:18 +0700

    New

--- app/src/main/java/com/example/lapstore/MainActivity.java
+++ app/src/main/java/com/example/lapstore/MainActivity.java
@@ -10,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        // Sao thang Thai ko bao t cai nay som hon
     }
 }
