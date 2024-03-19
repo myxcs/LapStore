3c8ff4e755a11ed1f15875d7ae31239f0cf27221
NgDTrong <trongndph23292@fpt.edu.vn>
2024-03-19 23:35:18 +0700

    trọng

--- app/src/main/java/com/example/lapstore/CartScreen.java
+++ app/src/main/java/com/example/lapstore/CartScreen.java
@@ -10,5 +10,6 @@ public class CartScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart_screen);
+        //trọng clone git
     }
 }
